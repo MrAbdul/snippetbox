33c5aaec2e3f1022dba7f51f81f92422eb7bd5db
abdulrahman <mrabdulrahman95@gmail.com>
2024-06-27 22:05:05 +0300

    Chapter 4.8. Multiple-record SQL queries we have to close the rows or else we will get a leak so we defer it.
    we also need to check the results of the error returned after iterating over the rows, since the loop compelting doesn't neccessery means that it was successful.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"snippetbox.abdulalsh.com/internal/models"
 	"strconv"
@@ -11,27 +10,38 @@ import (
 
 // this is the home handler which will write a byte slice contiaing the word hello from snippit  box
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	//2.6 you must ensure that header map contains all the headers you want before calling w.writeheader or w.write
-	//2.8 we can use the template here
-	//2.8 part 2 we can parse multiple template filesm
-	//		note that the file containg the base template must be first in the slice
-	fileslocations := []string{
-		"./ui/html/base.gohtml",
-		"./ui/html/pages/home.gohtml",
-		"./ui/html/partials/nav.gohtml",
-	}
-	files, err := template.ParseFiles(fileslocations...)
-	if err != nil {
-		app.serverError(w, r, err) // Use the serverError() helper.
-		return
-	}
-	//now that we have the file opened we can execute it
 
-	err = files.ExecuteTemplate(w, "base", nil)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
+	//2.6 you must ensure that header map contains all the headers you want before calling w.writeheader or w.write
+	//2.8 we can use the template here
+	//2.8 part 2 we can parse multiple template filesm
+	//		note that the file containg the base template must be first in the slice
+	//fileslocations := []string{
+	//	"./ui/html/base.gohtml",
+	//	"./ui/html/pages/home.gohtml",
+	//	"./ui/html/partials/nav.gohtml",
+	//}
+	//files, err := template.ParseFiles(fileslocations...)
+	//if err != nil {
+	//	app.serverError(w, r, err) // Use the serverError() helper.
+	//	return
+	//}
+	////now that we have the file opened we can execute it
+	//
+	//err = files.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, r, err)
+	//	return
+	//}
 
 }
 
